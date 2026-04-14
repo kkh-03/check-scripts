7bf0900f8223919185380a29caf333b7415b4554
GeunHyeong Kim <kkh99480@naver.com>
2026-04-15 04:11:49 +0900

    Script save

--- U-15script.sh
+++ U-15script.sh
@@ -5,7 +5,7 @@ sudo adduser --disabled-password --gecos "" testuser
 
 echo " testuser 홈 디렉토리 확인"
 
-sudo ls /home/testuser
+sudo ls -al /home/testuser
 
 echo "testuser 계정 삭제(홈은 남김)"
 sudo deluser testuser
