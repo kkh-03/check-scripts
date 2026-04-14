dc0e53cf847b8c61e53108a9819308c1bc24d942
GeunHyeong Kim <kkh99480@naver.com>
2026-04-15 03:50:08 +0900

    Script save2

--- U-15script.sh
+++ U-15script.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+echo " 테스트 계정 생성"
+sudo adduser --disabled-password --gecos "" testuser
+
+echo " testuser 홈 디렉토리 확인"
+
+ls /home/testuser
+
+echo "testuser 계정 삭제(홈은 남김)"
+sudo deluser testuser
+
 echo " 소유자가 존재지 않는 파일 및 디렉터리를 확인합니다"
-echo " 관리자 권한이 있는 계정에서 실행해주세요"
 
 sudo find / -nouser -o -nogroup 2> /dev/null
